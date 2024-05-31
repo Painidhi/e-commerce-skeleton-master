3c0c4ecfd7c3c973bfd3dd3029f899614c952dbe
unknown <210938@sdmcujire.in>
2024-05-31 12:10:54 +0530

    first push

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className='pr'>
+      <h1>ABOUT OUR PAGE</h1>
+      <h2>Shopping Cart is the easiest way to put your business online today! Start selling your products online in no time at all. No experience with web pages is necessary when you have our easy to use control panel, Products Listing with searching options, Images Zoom In or Out, Checkout with billing and shipping options and many more features.</h2> 
+  
+       
+     <img src='https://www.pngplay.com/wp-content/uploads/1/Shopping-Cart-PNG-Image.png' width="100%" height="450px"></img>
+   </div>
+  );
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className='pro'>
+      <h1>WELCOME TO OUR SHOPPING</h1>
+     
+    </div>
+  );
+}
+
+export default Home;
